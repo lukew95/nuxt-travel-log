ccf3461ff09eec598029134bf54fe40338b3413a
LukeWardDev <lukewarddev@gmail.com>
2025-07-05 11:07:31 +0100

    Added Layout/Index/Navbar

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { NuxtLink } from "#components";
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </Nuxtlink>
+    </div>
+
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In With Github
+        <Icon name="tabler:brand-github" size="24" />
+      </button>
+    </div>
+  </div>
+</template>
